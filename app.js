f2747c002a8182514b8b346743d91a2a57dfcbed
GuRuGu <peter.k.nets@gmail.com>
2020-01-19 09:47:03 +0200

    Add simple route

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const morgan = require("morgan");
 const config = require("config");
 
+const starsRoutes = require("./controllers");
+
 // Set up server app
 const app = express();
 
 // Set up middleware
 app.use(morgan("dev"));
+
+// Handle routing
+// app.use("/api/v1/stars", (req, res) => {
+//   res.json({ message: "Hello!" });
+// });
+app.route("/api/v1/stars").get(starsRoutes.getAllStars);
 
 // Run server
 const port = process.env.PORT || config.get("port");
---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // @route   GET api/v1/stars
+  // @desc    Get all stars
+  // @access  Public
+  getAllStars: (req, res) => {
+    res.send("Hello!");
+  }
+};
