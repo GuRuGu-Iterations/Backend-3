4113d7d69848f92f47602602e2e6e04b3288b2f9
GuRuGu <peter.k.nets@gmail.com>
2020-01-24 08:44:24 +0200

    Load main page and return it

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
+
+const mainPage = fs.readFileSync(`${__dirname}/../pages/main.html`, "utf-8");
+
 module.exports = {
   // @route   GET api/v1/stars
   // @desc    Get all stars
   // @access  Public
   getAllStars: (req, res) => {
-    res.send("Hello!");
+    res.send(mainPage);
   }
 };
